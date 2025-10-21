16c04191febc92f17077381c6c196d5484abaef9
Denzel-254 <ochiengden335@gmail.com>
2025-10-21 17:35:31 +0300

    Day 1:Initial setup, Tailwind and react icons configuration

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { SiActix } from 'react-icons/si';
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          <SiActix className="inline-block mr-5" />
+
+          Hello world!
+        </h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
