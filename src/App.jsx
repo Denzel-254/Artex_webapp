578c9760709e2eee59ae111425953609d1bbb1f4
Denzel-254 <ochiengden335@gmail.com>
2025-10-22 21:05:13 +0300

    Day 2:Created and styled layout,hero section and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import './App.css'
-import { SiActix } from 'react-icons/si';
+import Header from './components/layout/Header'
+import Hero from './components/sections/Hero'
+import Stats from './components/sections/Stats'
+import SectionWrapper from './components/common/Sectionwrapper'
 
 function App() {
 
 
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          <SiActix className="inline-block mr-5" />
-
-          Hello world!
-        </h1>
-      </div>
-
+      <Header />
+      <Hero />
+      <SectionWrapper className=" absolute top-160 left-0 right-0 mx-auto w-11/12 md:w-3/4 md:absolute md:top-125 md:left-0 md:right-0 md:mx-auto ">
+        <Stats />
+      </SectionWrapper>
     </>
   )
 }
 
 export default App
+
+
+
+
+
