7c9910fb12f64ab837c6054128712b14af69b7ed
Denzel-254 <ochiengden335@gmail.com>
2025-10-23 21:10:51 +0300

    Day 3: Added the expertise section and implimented it

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/layout/Header'
 import Hero from './components/sections/Hero'
 import Stats from './components/sections/Stats'
 import SectionWrapper from './components/common/Sectionwrapper'
+import Expertise from './components/sections/Expertise'
 
 function App() {
 
@@ -13,6 +14,10 @@ function App() {
       <Hero />
       <SectionWrapper className=" absolute top-160 left-0 right-0 mx-auto w-11/12 md:w-3/4 md:absolute md:top-125 md:left-0 md:right-0 md:mx-auto ">
         <Stats />
+      </SectionWrapper>
+
+      <SectionWrapper className="mt-8 bg-gray-50">
+        <Expertise />
       </SectionWrapper>
     </>
   )
