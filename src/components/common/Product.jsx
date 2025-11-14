0e3ff00b7b048db4bd335ed5017bda5f234ba368
Denzel-254 <ochiengden335@gmail.com>
2025-11-14 23:48:34 +0300

    Modified products section

--- src/components/common/Product.jsx
+++ src/components/common/Product.jsx
@@ -136,7 +136,7 @@ const Products = () => {
       </p>
 
       {/* Product grid */}
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 justify-items-center">
         {currentProducts.map((product) => (
           <div key={product.id} className="w-64">
             {/* Card */}
